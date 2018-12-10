3d28140f7cc41f57e71e028345c5b75a7858524d
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2018-12-09 23:39:41 -0600

    bed time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,12 @@ $(document).ready(function () {
 
     var yourHealth = 0;
     var enemyHealth = 0;
-    var yourDamage = 8;
+    var yourDamage = 0;
     var enemyDamage = 0;
-    var randomDamage = [10, 15, 20];
+    var randomDamage = [10, 12, 15];
+    var wins = 0;
+
+    $(".attackBtn").hide();
 
     //choose your fighter, then choose your enemy
     $(".card.fighter").on("click", function chooseFighter() {
@@ -21,7 +24,7 @@ $(document).ready(function () {
         yourHealth = $(this)
             .children(".card-body")
             .find("#health")
-            .text();     //selects the health of the fighter you chose and assigns it to the fghterHealth var
+            .text();                                      //selects the health of the fighter you chose and assigns it to the fghterHealth var
         $(this)
             .children(".card-body")
             .find("#health")
@@ -48,18 +51,19 @@ $(document).ready(function () {
         console.log("enemy health " + enemyHealth);
         enemyDamage = randomDamage[Math.floor(Math.random() * randomDamage.length)];        //randomly selects a damage for the enemy damage fromt he randomDamage array
         console.log("enemy damage " + enemyDamage);
-        $(document).off("click", ".card.enemy", chooseEnemy);       //stops the chooseFighter func so the user can't keep picking enemys until current is defeated
+        $(document).off("click", ".card.enemy", chooseEnemy);       //stops the chooseEnemy func so the user can't keep picking enemys until current is defeated        
+        if (yourHealth <= 0) {                                      //if you health is <= 0 you cannot choose a new enemy
+            $(document).off("click", ".card.enemy", chooseEnemy);
+        }
+        $(".attackBtn").show();
     }
     
     $(document).on("click", ".card.enemy", chooseEnemy);        //runs the chooseEnemy func on click following the chooseFighter func
-   
-
-    //now that cards are in correct locations, begin battle func
     
 
-        
-        //$(".hpText").text(enemyHealth);
-        
+    //now that cards are in correct locations, begin battle func
+
+    
     
     $(".attackBtn").on("click", function battle() {
         //console.log("battle!!!");
@@ -71,28 +75,42 @@ $(document).ready(function () {
         //console.log("you " + yourHealth);
         yourDamage = yourDamage + 8;
         //console.log("your damage " + yourDamage);
-        if (enemyHealth <= 0) {
-            alert("You won the duel! Choose a new enemy!");
-            $(".card.enemy").hide();
-            $(".card").addClass("enemy");
-        }
-        $(document).on("click", ".card.enemy", chooseEnemy);
-
-        if (yourHealth <= 0) {
-            alert("You lost the battle! Hit RESET to play again!");
-        }
+        $("#heroDamage").text(yourDamage);
+        $("#villanDamage").text(enemyDamage);
+        checkHealth();
     });
 
+    function checkHealth() {
+        if (enemyHealth <= 0) {
+            alert("You won the duel!");
+            $(".card.enemy").hide();
+            $(".attackBtn").hide();
+            $(".card").addClass("enemy");
+            wins = wins + 1;
+        }
+        
+        $(document).on("click", ".card.enemy", chooseEnemy);
+
+        if (yourHealth <= 0 && enemyHealth >= 0) {
+            alert("You lost the battle! Hit RESET to play again!");
+            $(".attackBtn").off("click");
+            $(".attackBtn").hide();
+            $(".card.fighter").hide();
+        }
     
-
-      
-
-    //enemyHealth - yourDamage = enemyHealth
-    //yourHealth - enemyDamage = yourHealth
-    //yourDamage + 8 = yourDamage
-    //no change to enemyDamage
-
-    //run on.("click") until enemyHealth <= 0. 
-    //then prompt user with alert to choose new enemy and run chooseEnemy func on click of enemy character
+        if (wins === 3) {
+        alert("Congratulations! You won the battle! Press RESET to play again!");
+        $(".attackBtn").hide();
+        }
+    }
+    
+    
+    $(".resetBtn").on("click", function reset() {
+        location.reload();
+    });
 
 });
+
+//TO DO LIST::
+//1. check the health after each click of the attack button, not instantly when clicked
+//2. Only display "choose new enemy" alert if there are still enemies left to choose
