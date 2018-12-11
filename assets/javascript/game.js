ae2513aa69640c855fc73942d71fe51a55f17936
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2018-12-10 20:52:43 -0600

    ready

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     var enemyHealth = 0;
     var yourDamage = 0;
     var enemyDamage = 0;
-    var randomDamage = [5, 8, 10, 12, 15];
+    var randomDamage = [5, 8, 10, 12, 15];              //random damages the enemys can deal to the user card
     var wins = 0;
 
     $(".attackBtn").hide();
@@ -50,7 +50,7 @@ $(document).ready(function () {
             .find("#health")
             .addClass("enemyHp");                       //locates the #health text of the card the user chose and adds the .enemyHp class
         console.log("enemy health " + enemyHealth);
-        enemyDamage = randomDamage[Math.floor(Math.random() * randomDamage.length)];        //randomly selects a damage for the enemy damage fromt he randomDamage array
+        enemyDamage = randomDamage[Math.floor(Math.random() * randomDamage.length)];        //randomly selects a damage for the enemy damage from the randomDamage array
         console.log("enemy damage " + enemyDamage);
         $(document).off("click", ".card.enemy", chooseEnemy);       //stops the chooseEnemy func so the user can't keep picking enemys until current is defeated        
         if (yourHealth <= 0) {                                      //if you health is <= 0 you cannot choose a new enemy
@@ -67,49 +67,49 @@ $(document).ready(function () {
 
     
     
-    $(".attackBtn").on("click", function battle() {
+    $(".attackBtn").on("click", function battle() {            //runs the battle func when the attackBtn is clicked
         //console.log("battle!!!");
-        enemyHealth = enemyHealth - yourDamage;
+        enemyHealth = enemyHealth - yourDamage;                 //subtracts the damage the user gives enemy from the enemyHealth and print it to the page
         $(".enemyHp").text(enemyHealth);
         //console.log("enemy " + enemyHealth);
-        yourHealth = yourHealth - enemyDamage;
+        yourHealth = yourHealth - enemyDamage;              //subtract the damage the user receives from the enemy and prints it to the page
         $(".yourHp").text(yourHealth);
         //console.log("you " + yourHealth);
-        yourDamage = yourDamage + 8;
+        yourDamage = yourDamage + 8;                        //adds 8 damage points to grow the user damage
         //console.log("your damage " + yourDamage);
-        $("#heroDamage").text(yourDamage);
-        $("#villanDamage").text(enemyDamage);
-        checkHealth();
+        $("#heroDamage").text(yourDamage);                  //tells the user how much damage they are dealing the enemy
+        $("#villanDamage").text(enemyDamage);               //tells the user how much damage they are receiving
+        checkHealth();                                      //runs checkHealth func
     });
 
-    function checkHealth() {
-        if (enemyHealth <= 0) {
+    function checkHealth() {    
+        if (enemyHealth <= 0) {                     
             alert("You won the duel!");
-            $(".card.enemy").hide();
+            $(".card.enemy").hide();                        //once and enemy is defeated, their card disapears
             $(".attackBtn").hide();
-            $(".card").addClass("enemy");
-            $("#continue").show();
-            wins = wins + 1;
+            $(".card").addClass("enemy");                   //adds the enemy class to the remaining cards so they can be clicked
+            $("#continue").show();                          //shows a message requesting the user to select a new enemy
+            wins = wins + 1;                                //increases wins var by 1
         }
         
-        $(document).on("click", ".card.enemy", chooseEnemy);
+        $(document).on("click", ".card.enemy", chooseEnemy);        //runs the choose enemy func when enemy card is clicked. 
 
-        if (yourHealth <= 0 && enemyHealth >= 0) {
-            alert("You lost the battle! Hit RESET to play again!");
+        if (yourHealth <= 0 && enemyHealth >= 0) {                          //if user health is under 0 and enemy still has health, game over
+            alert("You lost the battle! Hit RESET to play again!");         //prompt user to click resetBtn
             $(".attackBtn").off("click");
             $(".attackBtn").hide();
-            $(".card.fighter").hide();
+            $(".card.fighter").hide();                                      //hide the user card
         }
     
-        if (wins === 3) {
-        alert("Congratulations! You won the battle! Press RESET to play again!");
+        if (wins === 3) {                                                 //when user reaches 3 wins
+        alert("Congratulations! You won the battle! Press RESET to play again!");          //prompt user to hit resetBtn for new game 
         $(".attackBtn").hide();
         $("#continue").hide();
         }
     }
     
     
-    $(".resetBtn").on("click", function reset() {
+    $(".resetBtn").on("click", function reset() {               //when resetBtn clicked, game resets to default settings. 
         location.reload();
     });
 
